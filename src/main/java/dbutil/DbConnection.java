0b8c2287dc66b5273c7479bf9eab0377bedd4599
CharlieGoodson <max.sadchikov@gmail.com>
2017-09-02 15:57:17 +0300

    Login form - done!

--- src/main/java/dbutil/DbConnection.java
+++ src/main/java/dbutil/DbConnection.java
@@ -1,4 +1,20 @@
 package dbutil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+
+    private static final String SQCONN = "jdbc:sqlite:/db/schoolsystem.sqlite";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
